d7ffe79917314d7cbbd69626bdbd1d7c7a8a941f
hidylima <hidy63@gmail.com>
2018-11-28 22:59:53 -0200

    html finalizado

--- assets/js/components/dooutss-navegatios.js
+++ assets/js/components/dooutss-navegatios.js
@@ -1,14 +1,11 @@
-
 (function() {
-  var $menuToogle = document.querySelector("#menuToogle");
+  const $menuToogle = document.querySelector("#menuToogle");
   var $navigation = document.querySelector("#dooutss-navigation");
   var $item = $navigation.querySelector(".item");
 
-  $menuToogle.addEventListener("click", function event(event){
+  $menuToogle.addEventListener("click", function event(event) {
     event.preventDefault();
     $menuToogle.classList.toggle("-active");
     $navigation.classList.toggle("-active");
-    
   });
-  
 })();
